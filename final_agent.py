3dcbc9a3eb7a3ff08b8ac64a2655d649b98aff57
DSDigitalHub1637 <daoudasanou6737@gmail.com>
2026-06-01 16:19:47 +0000

    Update final_agent.py

--- final_agent.py
+++ final_agent.py
@@ -9,7 +9,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 logo_path = os.path.join(current_dir, "logo_ds.png")
 
 # Configuration du tunnel n8n
-N8N_WEBHOOK_URL = https://primary-production-b36e9.up.railway.app/webhook-test/samira-whatsapp
+N8N_WEBHOOK_URL =https://primary-production-b36e9.up.railway.app/webhook-test/samira-whatsapp
 
 def notifier_n8n(texte_samira, client_prompt):
     """Envoie discrètement la réponse de Samira vers ton serveur n8n."""
